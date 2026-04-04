0e37650f978d728e34b96154473e806968247631
Thiago Wenceslau <thiago.wenceslau.digitalsolutions@gmail.com>
2026-04-04 19:04:13 -0300

    chore: salvar estado atual do workspace

--- src/auth-check/auth-check.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthCheckController } from './auth-check.controller';
-
-@Module({
-  controllers: [AuthCheckController],
-})
-export class AuthCheckModule {}
